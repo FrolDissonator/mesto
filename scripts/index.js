b60d8d134d821f1ad13730169068ce90a3a3524d
Sergey Frolov <froldissonator@yandex.ru>
2023-03-27 01:32:32 +0200

    Make add cards function

--- scripts/index.js
+++ scripts/index.js
@@ -1,17 +1,32 @@
-const popupElement = document.querySelector(".popup_profile");
-const popupCardElement = document.querySelector(".popup_card");
-const popupAddCardElement = document.querySelector(".popup_card");
-const popupCloseButtonElement = popupElement.querySelector(".popup__close-button");
-const popupOpenButtonElement = document.querySelector(".profile__edit-button");
-const popupCardOpenButtonElement = document.querySelector(".profile__add-button");
-const formElement = document.querySelector(".edit-form");
-let nameInput = formElement.querySelector(".edit-form__input_field_name");
-let jobInput = formElement.querySelector(".edit-form__input_field_description");
-let profileName = document.querySelector(".profile__title");
-let profileDescription = document.querySelector(".profile__subtitle");
+// popups
+const popupProfile = document.querySelector(".popup_profile");
+const popupCard = document.querySelector(".popup_card");
+
+// popups close buttons
+const popupProfileCloseButton = popupProfile.querySelector(".popup__close-button");
+const popupCardCloseButton = popupCard.querySelector(".popup__close-button");
+
+//popups open buttons
+const popupProfileOpenButton = document.querySelector(".profile__edit-button");
+const popupCardOpenButton = document.querySelector(".profile__add-button");
+
+// edit form constants
+const formProfile = document.querySelector(".edit-form_profile");
+const nameInput = formProfile.querySelector(".edit-form__input_field_name");
+const jobInput = formProfile.querySelector(".edit-form__input_field_description");
+const profileName = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__subtitle");
+
+// add form constants
+const formCard = document.querySelector(".edit-form_card");
+const placeNameInput = formCard.querySelector(".edit-form__input_field_place-name");
+const placeImageInput = formCard.querySelector(".edit-form__input_field_place-image");
+
+// cards constants
 const cardsGrid = document.querySelector(".cards-grid");
 const cardTemplate = document.getElementById("card").content;
 
+// start cards array
 const initialCards = [
   {
     name: 'Архыз',
@@ -39,7 +54,8 @@ const initialCards = [
   }
 ];
 
-initialCards.forEach((data) => {
+// cards creation functions
+function createCard (data) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
   const placeImage = cardElement.querySelector(".card__image");
   const placeTitle = cardElement.querySelector(".card__title");
@@ -50,31 +66,58 @@ initialCards.forEach((data) => {
 
   cardsGrid.append(cardElement);
   setEventListeners(cardElement);
+
+  return cardElement;
+};
+
+initialCards.forEach (elem => {
+  createCard(elem);
 });
 
+function handleAddCard (evt) {
+  evt.preventDefault();
+
+  const newCard = createCard({
+    name: placeNameInput.value,
+    link: placeImageInput.value
+  });
+
+  cardsGrid.prepend(newCard);
+  evt.target.reset();
+  closePopup(popupCard);
+};
+
+
+// cards deletion function
 function handleDelete (evt) {
   const card = evt.target.closest(".card");
   card.remove();
 };
 
+// cards like function
 function addLike (evt) {
   const like = evt.target.closest(".card").querySelector(".card__like-button");
   like.classList.toggle("card__like-button_active");
 };
 
+// listeners for cards functions
 function setEventListeners (cardElement) {
   cardElement.querySelector(".card__delete-button").addEventListener("click", handleDelete);
   cardElement.querySelector(".card__like-button").addEventListener("click", addLike);
 };
 
-const openPopup = function () {
+formCard.addEventListener("submit", handleAddCard);
+
+// popups functions
+const openPopup = function (popup) {
   nameInput.value = profileName.textContent;
   jobInput.value = profileDescription.textContent;
-  popupElement.classList.add("popup_opened");
+
+  popup.classList.add("popup_opened");
 };
 
-const closePopup = function () {
-  popupElement.classList.remove("popup_opened");
+const closePopup = function (popup) {
+  popup.classList.remove("popup_opened");
 };
 
 function handleFormSubmit (evt) {
@@ -83,11 +126,25 @@ function handleFormSubmit (evt) {
   profileName.textContent = nameInput.value;
   profileDescription.textContent = jobInput.value;
 
-  closePopup();
-}
+  closePopup(popupProfile);
+};
 
 
-formElement.addEventListener('submit', handleFormSubmit);
-popupOpenButtonElement.addEventListener("click", openPopup);
-popupCloseButtonElement.addEventListener("click", closePopup);
-popupCardOpenButtonElement.addEventListener("click", openPopup);
+// listeners for popups functions
+formProfile.addEventListener("submit", handleFormSubmit);
+
+popupProfileOpenButton.addEventListener("click", function () {
+  openPopup(popupProfile);
+});
+
+popupProfileCloseButton.addEventListener("click", function () {
+  closePopup(popupProfile);
+});
+
+popupCardOpenButton.addEventListener("click", function () {
+  openPopup(popupCard);
+});
+
+popupCardCloseButton.addEventListener("click", function () {
+  closePopup(popupCard);
+});
